8fd812bd163a27fbb434003cb654bb196e5cfcaa
Jobarah <barahona.josue@unitec.edu>
2017-02-23 01:28:46 -0600

    Replace

---
+++ Replace/cmake-build-debug/CMakeFiles/Replace.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Replace.dir/main.cpp.obj"
+  "CMakeFiles/Replace.dir/Test.cpp.obj"
+  "Replace.pdb"
+  "Replace.exe"
+  "Replace.exe.manifest"
+  "libReplace.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Replace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
